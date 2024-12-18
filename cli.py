49dd0a83652cd57a4482e35e74c8384a4339677f
masinde sylvester <masinde.sylvester@yahoo.com>
2024-12-18 15:29:17 +0300

    add update doctors records

--- cli.py
+++ cli.py
@@ -16,7 +16,7 @@ def init_database():
     print("Database created and tables initialized.")
 
 # create database
-def create_doctors():
+def create_doctor():
     name = input("Enter Doctor's name")
     age = int(input("Enter Doctor's age"))
     phone_number = input("Enter Doctor's phone number")
@@ -24,3 +24,6 @@ def create_doctors():
     hospital_id = int(input("Enter Doctor's hospital id"))
     department = int(input("Enter Doctor's department"))
     doctor = Doctor(name=name, age=age, phone_number=phone_number, email=email, hospital_id=hospital_id, department=department)
+    session.add(doctor)
+    session.commit()
+    print(f"Doctor '{name}' created successfully with '{doctor.id}'")
