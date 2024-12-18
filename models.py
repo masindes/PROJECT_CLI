f92effd7ca18ff5fdb9247f07a3bbaaff0e09065
masinde sylvester <masinde.sylvester@yahoo.com>
2024-12-18 13:56:00 +0300

    initial commit

--- models.py
+++ models.py
@@ -12,8 +12,22 @@ class Doctor(Base):
     phone_number= Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     hospital_id = Column(Integer, nullable = False )
+    department = Column(Integer, nullable = False )
 
     patients = relationship('Patient', back_populates='doctor')
 
     def __repr__(self):
-        return f'<Doctor(id={self.id}, name={self.name}, phone_number={self.phone_number}, email={self.email}, hospital_id={self.hospital_id})>'
+        return f'<Doctor(id={self.id}, name={self.name}, phone_number={self.phone_number}, email={self.email}, hospital_id={self.hospital_id}, department={self.department})>'
+    
+
+class Patient (Base):
+    __tablename__ = 'patients'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    phone_number = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    adm_number = Column(String, nullable=False)
+    ward_name = Column(String, nullable=False)
+    
+    doctor_id = Column(Integer, ForeignKey('doctors.id'), nullable=False)
+    
