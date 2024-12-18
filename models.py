7bb76c7f42cfa9e7beb9a01eba1f01dee52314e8
masinde sylvester <masinde.sylvester@yahoo.com>
2024-12-18 13:47:28 +0300

    initial commit

---
+++ models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String,ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+
+
+class Doctor(Base):
+    __tablename__ = 'doctors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    phone_number= Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    hospital_id = Column(Integer, nullable = False )
+
+    patients = relationship('Patient', back_populates='doctor')
+
+    def __repr__(self):
+        return f'<Doctor(id={self.id}, name={self.name}, phone_number={self.phone_number}, email={self.email}, hospital_id={self.hospital_id})>'
