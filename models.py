02c0e669995efa664f8e53aebe2df7e40a54450b
masinde sylvester <masinde.sylvester@yahoo.com>
2024-12-18 14:07:25 +0300

    initial commit

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ class Doctor(Base):
     __tablename__ = 'doctors'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
     phone_number= Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     hospital_id = Column(Integer, nullable = False )
@@ -17,17 +18,22 @@ class Doctor(Base):
     patients = relationship('Patient', back_populates='doctor')
 
     def __repr__(self):
-        return f'<Doctor(id={self.id}, name={self.name}, phone_number={self.phone_number}, email={self.email}, hospital_id={self.hospital_id}, department={self.department})>'
+        return f'<Doctor(id={self.id}, name={self.name}, age={self.age}, phone_number={self.phone_number}, email={self.email}, hospital_id={self.hospital_id}, department={self.department})>'
     
 
 class Patient (Base):
     __tablename__ = 'patients'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False) 
     phone_number = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     adm_number = Column(String, nullable=False)
     ward_name = Column(String, nullable=False)
-    
     doctor_id = Column(Integer, ForeignKey('doctors.id'), nullable=False)
+
+    doctor = relationship('Doctor', back_populates='patients')
+
+    def __repr__(self):
+        return f'<Patient(id={self.id}, name={self.name}, age={self.age}, phone_number={self.phone_number}, email={self.email}, adm_number={self.adm_number}, ward_name={self.ward_name}, doctor_id={self.doctor_id})>'
     
