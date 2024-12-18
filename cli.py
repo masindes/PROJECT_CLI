82a7b3a26c02e2704e7afcbce1b0af74d07547a1
masinde sylvester <masinde.sylvester@yahoo.com>
2024-12-18 15:20:00 +0300

    initial commit

--- cli.py
+++ cli.py
@@ -8,3 +8,19 @@ DATABASE_URL = 'sqlite://doctors.db'
 
 engine = create_engine(DATABASE_URL)
 session = sessionmaker(bind=engine)
+session = sessionmaker()
+
+# initialize database
+def init_database():
+    Base.metadata.create_all(engine)
+    print("Database created and tables initialized.")
+
+# create database
+def create_doctors():
+    name = input("Enter Doctor's name")
+    age = int(input("Enter Doctor's age"))
+    phone_number = input("Enter Doctor's phone number")
+    email = input("Enter Doctor's email")
+    hospital_id = int(input("Enter Doctor's hospital id"))
+    department = int(input("Enter Doctor's department"))
+    doctor = Doctor(name=name, age=age, phone_number=phone_number, email=email, hospital_id=hospital_id, department=department)
